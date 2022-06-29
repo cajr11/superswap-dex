bb0fcf71a964c7eafaf7d96c88cb88736c51b74c
cajr11 <caondo@yahoo.co.uk>
2022-06-29 09:51:39 -0500

    add initial token selector button style

--- src/components/SwapForm/SwapFormChangeTokenButton.tsx
+++ src/components/SwapForm/SwapFormChangeTokenButton.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import ethLogo from "../../assets/images/eth.png";
 import { useTranslation } from "react-i18next";
 
-
 type SwapFormChangeTokenButtonProps = {
-    initial?: string
-}
+  initial?: boolean;
+};
 
 const SwapFormChangeTokenButton = ({ initial }: SwapFormChangeTokenButtonProps) => {
-
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
-    <button className="flex whitespace-nowrap ml-1 border rounded-3xl p-2">
-          {initial && <img src={ethLogo} alt="token logo" className="h-6 w-6 mr-3" />}
-          <span className="flex items-center">
-            {initial ? "ETH" : t("swap_form.select")}
-          <ChevronDownIcon className={`h-4 w-4 ${initial && "mr-2"}`}/>
-          </span>
+    <button className={`flex whitespace-nowrap ml-1 rounded-3xl p-2 ${initial ? "bg-gray-200" : "bg-orange-400 text-white"}`}>
+      {initial && <img src={ethLogo} alt="token logo" className="h-6 w-6 mr-3" />}
+      <span className="flex items-center ml-2">
+        {initial ? "ETH" : t("swap_form.select")}
+        <ChevronDownIcon className={`h-4 w-4 ${initial && "mr-2"}`} />
+      </span>
     </button>
-  )
-}
+  );
+};
 
-export default SwapFormChangeTokenButton
+export default SwapFormChangeTokenButton;
