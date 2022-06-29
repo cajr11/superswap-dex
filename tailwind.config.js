25bc5b4cf6c0a5e1f90f594b919e8d9d386d6a7a
cajr11 <caondo@yahoo.co.uk>
2022-06-29 08:13:53 -0500

    add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      inc: ["Roboto Mono", "monospace"],
+    },
   },
   plugins: [],
 };
