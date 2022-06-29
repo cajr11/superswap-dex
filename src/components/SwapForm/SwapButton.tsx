71d9718bfd9ee54e04ba19e9eebe6bc98b07a82f
cajr11 <caondo@yahoo.co.uk>
2022-06-29 09:50:57 -0500

    change connect wallet button text color

--- src/components/SwapForm/SwapButton.tsx
+++ src/components/SwapForm/SwapButton.tsx
@@ -6,7 +6,7 @@ const SwapButton = (): JSX.Element => {
 
   return (
     <div className="border-orange-300 h-16 w-full rounded-3xl">
-      <button className="h-full w-full rounded-3xl flex justify-center items-center bg-orange-300 text-orange-600 font-semibold">
+      <button className="h-full w-full rounded-3xl flex justify-center items-center bg-orange-300 text-white font-semibold">
         {t("swap_form.connect")}
       </button>
     </div>
