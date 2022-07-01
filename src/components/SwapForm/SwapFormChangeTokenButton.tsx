fb20df13ca8e8637ad96be42e1053df4332cb85e
cajr11 <caondo@yahoo.co.uk>
2022-07-01 10:46:13 -0500

    correct swap form responsiveness on medium screens

--- src/components/SwapForm/SwapFormChangeTokenButton.tsx
+++ src/components/SwapForm/SwapFormChangeTokenButton.tsx
@@ -12,8 +12,8 @@ const SwapFormChangeTokenButton = ({ initial }: SwapFormChangeTokenButtonProps):
 
   return (
     <button className={`flex whitespace-nowrap ml-1 rounded-3xl p-2 ${initial ? "bg-gray-200" : "bg-orange-400 text-white"}`}>
-      {initial && <img src={ethLogo} alt="token logo" className="h-6 w-6 mr-3" />}
-      <span className="flex items-center ml-2">
+      {initial && <img src={ethLogo} alt="token logo" className="h-6 w-6" />}
+      <span className={`flex items-center ml-2 ${initial && "pr-2"} text-sm md:text-base`}>
         {initial ? "ETH" : t("swap_form.select")}
         <ChevronDownIcon className={`h-4 w-4 ${initial && "mr-2"}`} />
       </span>
