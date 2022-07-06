373a32ba9c24f391d93543378519f672ff05d538
cajr11 <caondo@yahoo.co.uk>
2022-07-05 20:16:56 -0500

    add token list types

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,12 @@
 export type LanguageType = "English" | "Spanish";
+
+export type TokenDetailsType = {
+    logoURI: string;
+    decimals: number;
+    name: string;
+    symbol: string;
+    tags: string[];
+    address: string;
+}
+
+export type TokenList = TokenDetailsType[]
